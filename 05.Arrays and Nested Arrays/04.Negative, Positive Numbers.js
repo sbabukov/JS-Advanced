4d641e3de366012c2174706c6ddc351bfb9d938d
sbabukov <78176545+sbabukov@users.noreply.github.com>
2024-01-04 11:48:13 +0200

    Create 04.Negative, Positive Numbers.js
    Negative, Positive Numbers

---
+++ 05.Arrays and Nested Arrays/04.Negative, Positive Numbers.js
@@ -0,0 +1,14 @@
+function solve(array) {
+    let result = [];
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] < 0) {
+            result.unshift(array[i]);
+        } else {
+            result.push(array[i]);
+        }
+
+    }
+    console.log(result.join('\n'));
+}
+
+solve([0, 3, -2,  -1])
