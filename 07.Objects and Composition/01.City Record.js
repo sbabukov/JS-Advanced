7483e94c5e0f4b30e5fe9ca1a856413d13de1509
sbabukov <78176545+sbabukov@users.noreply.github.com>
2024-01-07 16:16:22 +0200

    Create 01.City Record.js
    01.City Record

---
+++ 07.Objects and Composition/01.City Record.js
@@ -0,0 +1,15 @@
+function solve(city, population, treasury) {
+    // first way
+    let record = {
+        name: city,
+        population: population,
+        treasury: treasury,
+    }
+
+    
+}
+
+solve('Tortuga',
+7000,
+15000
+)
